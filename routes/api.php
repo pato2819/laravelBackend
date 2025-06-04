3352ee758f7c067ffa753adc8d06535a3fa322a2
patricia canul <patricia.canul@innovasport.com>
2025-06-03 22:50:26 -0600

    routas

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,6 @@ use App\Http\Controllers\PruebaController;
 
 Route::get('/saludos', [PruebaController::class, 'saludarGet']);
 Route::post('/saludos', [PruebaController::class, 'saludarPost']);
+Route::get('/health', function () {
+    return response()->json(['status' => 'ok'], 200);
+});
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use App\Http\Controllers\PruebaController;
 // Rutas API para saludar
 Route::get('/api/saludo', [PruebaController::class, 'saludarGet']);
 Route::post('/api/saludo', [PruebaController::class, 'saludarPost']);
+
+// Ruta de health check
+Route::get('/api/health', function () {
+    return response()->json(['status' => 'ok'], 200);
+});
